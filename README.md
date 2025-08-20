eb9587248898bad6ec2ab600651bb3aca7c7465e
15ho <fifteenho@163.com>
2025-08-20 14:32:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# XAuth
+> NOTE: This project is under development.
+
+The TOTP Authenticator is a Chrome extension that generates verification codes.
+
+base on:
+- [React](https://reactjs.org/)
+- [Vite](https://vitejs.dev/)
+- [CRXJS](https://crxjs.dev/vite-plugin)
+
+## TODO
+- [ ] security
+- [ ] theme
+- [ ] i18n
+- [ ] logo
---
+++ src/popup/pages/LockScreenPage.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { itemLockPasswd } from '../constants/storage'
+
+export default function LockScreenPage(props: { onUnlock: () => void }) {
+	const [passwd, setPasswd] = useState('')
+	const [error, setError] = useState('')
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const storagePasswd = localStorage.getItem(itemLockPasswd)
+		if (!storagePasswd) {
+			return
+		}
+		if (storagePasswd !== passwd) {
+			setError('Invalid password')
+			return
+		}
+		props.onUnlock()
+	}
+
+	return (
+		<div className="auth-container">
+			<div className="auth-box">
+				<h1> Your TOTP Authenticator </h1>
+
+				<form onSubmit={handleSubmit}>
+					<div className="input-group">
+						<input
+							type="password"
+							onChange={(e) => {
+								setPasswd(e.target.value)
+								setError('')
+							}}
+							placeholder="Please enter your password"
+							required
+						/>
+						<button type="submit">Unlock</button>
+					</div>
+
+					{error && <div className="error">{error}</div>}
+				</form>
+
+			</div>
+		</div>
+	)
+}
