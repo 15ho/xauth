c054f0250695460f7dc789936a68f86232d4ee94
15ho <fifteenho@163.com>
2025-08-27 12:28:32 +0800

    feat: add lock screen page style

--- src/popup/pages/LockScreenPage.tsx
+++ src/popup/pages/LockScreenPage.tsx
@@ -1,46 +1,58 @@
 import { useState } from 'react'
 import { storageKeyLockPasswd } from '../constants/storage'
+import { Button, CssBaseline, TextField } from '@mui/material'
 
 export default function LockScreenPage(props: { onUnlock: () => void }) {
-	const [passwd, setPasswd] = useState('')
-	const [error, setError] = useState('')
+  const [passwd, setPasswd] = useState('')
+  const [error, setError] = useState('')
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		const storagePasswd = localStorage.getItem(storageKeyLockPasswd)
-		if (!storagePasswd) {
-			return
-		}
-		if (storagePasswd !== passwd) {
-			setError('Invalid password')
-			return
-		}
-		props.onUnlock()
-	}
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const storagePasswd = localStorage.getItem(storageKeyLockPasswd)
+    if (!storagePasswd) {
+      return
+    }
+    if (storagePasswd !== passwd) {
+      setError('Invalid password')
+      return
+    }
+    props.onUnlock()
+  }
 
-	return (
-		<div className="auth-container">
-			<div className="auth-box">
-				<h1> Your TOTP Authenticator </h1>
+  return (
+    <>
+      <CssBaseline />
+      <h1> XAuth </h1>
 
-				<form onSubmit={handleSubmit}>
-					<div className="input-group">
-						<input
-							type="password"
-							onChange={(e) => {
-								setPasswd(e.target.value)
-								setError('')
-							}}
-							placeholder="Please enter your password"
-							required
-						/>
-						<button type="submit">Unlock</button>
-					</div>
+      <form onSubmit={handleSubmit} style={{ marginTop: '100px' }}>
+        <TextField
+          label="Enter your password"
+          variant="outlined"
+          value={passwd}
+          onChange={(e) => {
+            setPasswd(e.target.value)
+          }}
+          error={!!error}
+          helperText={error}
+          type="password"
+          sx={{ width: '90%' }}
+        />
 
-					{error && <div className="error">{error}</div>}
-				</form>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{
+            marginTop: '160px',
+            width: '88%',
+            height: '45px',
+            fontSize: '15px',
+            borderRadius: '18px'
+          }}
+          disabled={!passwd}>
+          Unlock
+        </Button>
 
-			</div>
-		</div>
-	)
+      </form>
+    </>
+  )
 }
